6732906d8f4ba8fcd390daa6cbed47ea91f95211
Nifty <102737556+JohannxH@users.noreply.github.com>
2024-07-18 22:36:31 +0200

    Update dynamicTitle.js
    - Update icons

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -11,10 +11,11 @@ window.onload = function () {
   function toggle(isPageActive) {
     if (isPageActive) {
       document.title = pageTitle
-      favicon.href = './assets/images/dp_male.svg'
+      favicon.href = './assets/images/1694372954847.jpg'
     } else {
       document.title = attentionMessage
-      favicon.href = './assets/images/folded.png'
+      //favicon.href = './assets/images/folded.png'
+      favicon.href = './assets/images/1694372954847.jpg'
     }
   }
 }
