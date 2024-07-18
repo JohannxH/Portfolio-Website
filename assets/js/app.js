80834f1db0abaaf9eb57e5904e5666a480a48570
Nifty <102737556+JohannxH@users.noreply.github.com>
2024-07-18 18:03:19 +0200

    Update app.js
    - Update quote

--- assets/js/app.js
+++ assets/js/app.js
@@ -118,16 +118,14 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-        <div>
-          <div>
-              <div class="tag">if (brain != empty)</div>
-              <div class="tag">{</div>
-              <div class="tag">  keepCoding();</div>
-              <div class="tag">}</div>
-              <div class="tag">else{</div>
-              <div class="tag">  orderCoffee();</div>
-              <div class="tag">}</div>
-          </div>
+        <div style="display: flex; flex-direction: column; align-items: center;">
+            <div class="tag">if (brain != empty)</div>
+            <div>{</div>
+            <div>  keepCoding();</div>
+            <div>}</div>
+            <div>else{</div>
+            <div>  orderCoffee();</div>
+            <div>}</div>
         </div>
     </div>
 
