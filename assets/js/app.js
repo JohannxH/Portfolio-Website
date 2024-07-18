0c30279a59b27c5d53a0260915a8a8e298728dfa
Nifty <102737556+JohannxH@users.noreply.github.com>
2024-07-18 18:19:45 +0200

    Update app.js
    - Update quote

--- assets/js/app.js
+++ assets/js/app.js
@@ -113,24 +113,13 @@ let header = $(`
 let footer = $(`
 <footer class="footer sticky-bottom"  style="background-color:#2b2a2a;">
   <div class="p-4">
-<div class="container-fluid quote-container">
-    <div class="quotes" style="text-align:center;">
-        <!--SVG code for quote-left icon-->
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
+    <div class="container-fluid quote-container">
+      <div class="quotes"    style="text-align:center;">
+      <!--SVG code for quote-left icon-->
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
+      </div>
+      <p class="tag"> < Happiness is when your code runs without errors ?> </p>
     </div>
-    <div style="display: flex; flex-direction: column; align-items: center;">
-        <div>
-            <pre class="tag" style="margin: 0; text-align: center;">if (brain != empty)
-            {
-              keepCoding();
-            }
-            else{
-              orderCoffee();
-            }
-            </pre>
-        </div>
-    </div>
-</div>
 
 
     
